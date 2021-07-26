d21a2eade83c0cc510a50c925a599a32cdfb5879
Magdalena Łozińska <45043996+mcgdc@users.noreply.github.com>
2021-07-26 18:04:43 +0200

    http-client

--- src/app/most-likely-qs.service.ts
+++ src/app/most-likely-qs.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+import { Question } from './question';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MostLikelyQsService {
+  private questionsUrl: string = 'http://najbardziejprawdopodobny.pl/api/';
+  httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  };
 
-  constructor() { }
+  getRandomQuestion(): Observable<Question>{
+    return this.http.get<Question>('http://najbardziejprawdopodobny.pl/api/question/random/');
+  }
+
+  constructor(private http: HttpClient) { }
 }
